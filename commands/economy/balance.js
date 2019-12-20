463ad01818f1b407b766fe407c816237869cff37
Sibahia <52184360+Sibahia@users.noreply.github.com>
2019-12-19 20:07:13 -0400

    Update balance.js

--- commands/economy/balance.js
+++ commands/economy/balance.js
@@ -42,33 +42,3 @@ config: {
 }
 
 
-/*exports.help = {
-  name: 'balance',
-  aliases: ['bal', 'balance']
-};*/
-
-/*const balance = require("../balance.json")
-const config = require("discord.js")
-const fs = require("fs")
-const Discord = require("discord.js")
-
-exports.run = (client, message, args) => {
-
-balance[message.mentions.users.first() || message.author] = {
-    level: 0,
-    cent: 0,
-    zafiro: 0
-}
-
-let user = message.mentions.users.first() || message.author;
-let cent = balance[message.mentions.users.first() || message.author].cen
-let zafiro = balance[message.mentions.users.first() || message.author].zafiro
-
-const embed = new Discord.RichEmbed()
-.setColor(config.colorT)
-.setThumbnail(user.avatarURL)
-.setDescription("Cent : " + cent + " \n Zafiro : " + zafiro)
-
-message.channel.send(embed)
-
-}*/
